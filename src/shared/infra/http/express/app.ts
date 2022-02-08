3b33ab4110b0b6a864458197f0ded4fa0975a620
Henrique Costa dos Santos <Henrique.Santos@tutiplast.local>
2022-02-08 08:24:24 -0400

    dev - inicial v4

--- src/shared/infra/http/express/app.ts
+++ src/shared/infra/http/express/app.ts
@@ -2,8 +2,10 @@ import "dotenv/config";
 import { port } from "@config/ServerConfig";
 import express from "express";
 import "express-async-errors";
+import swaggerUi from "swagger-ui-express";
 import { ErrorMiddleware } from "@shared/middlewares/ErrorMiddleware";
 import { routes } from "@shared/routes";
+import swaggerOptions from "../../../../swagger.json";
 
 interface IApp {
     start: () => void;
@@ -13,13 +15,14 @@ export function App(): IApp {
     const app = express();
 
     app.use(express.json()); // Configuração pra aceitar json
-    app.use(routes);
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerOptions)); // Configuração swagger para documentação
+    app.use(routes); // Configuração das rotas da aplicação
     app.use(ErrorMiddleware); // Tratativa de erro
 
     const start = () => {
-        app.listen(port, () =>
-            console.log(`Server is running in port ${port}`),
-        );
+        app.listen(port, () => {
+            console.log(`Server is running in port ${port}`);
+        });
     }; // Função que executa o servidor
 
     return { start };
